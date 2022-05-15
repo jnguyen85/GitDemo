73fa2fb6ceafe77cf2dfbe668b05c512fc067d5e
John <nguyen8585@gmail.com>
2022-05-15 14:21:34 -0700

    American guy commit

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -11,7 +11,7 @@ public class SeleniumTest {
 	{
 		System.out.println("BrowserAUtomation");	
 		
-		System.out.println("Web automation");	
+		System.out.println("MyWeb automation");	
 	}
 	@Test
 	public void elementsUi()
